f2d6a3282f8e499a6316d69f958eebcf4cc663e4
Sameer-05 <108287986+Sameer-05@users.noreply.github.com>
2022-07-04 12:56:49 +0530

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,20 +1,32 @@
-
+// See the Cormen book for details of the
+// following algorithm
 #include <bits/stdc++.h>
 using namespace std;
 
+// Matrix Ai has dimension p[i-1] x p[i]
+// for i = 1..n
 int MatrixChainOrder(int p[], int n)
 {
 
-	
+	/* For simplicity of the program, one
+	extra row and one extra column are
+	allocated in m[][]. 0th row and 0th
+	column of m[][] are not used */
 	int m[n][n];
 
 	int i, j, k, L, q;
 
-	
+	/* m[i, j] = Minimum number of scalar
+	multiplications needed to compute the
+	matrix A[i]A[i+1]...A[j] = A[i..j] where
+	dimension of A[i] is p[i-1] x p[i] */
+
+	// cost is zero when multiplying
+	// one matrix.
 	for (i = 1; i < n; i++)
 		m[i][i] = 0;
 
-	
+	// L is chain length.
 	for (L = 2; L < n; L++)
 	{
 		for (i = 1; i < n - L + 1; i++)
@@ -23,7 +35,7 @@ int MatrixChainOrder(int p[], int n)
 			m[i][j] = INT_MAX;
 			for (k = i; k <= j - 1; k++)
 			{
-				
+				// q = cost/scalar multiplications
 				q = m[i][k] + m[k + 1][j]
 					+ p[i - 1] * p[k] * p[j];
 				if (q < m[i][j])
@@ -35,23 +47,20 @@ int MatrixChainOrder(int p[], int n)
 	return m[1][n - 1];
 }
 
-
 int main()
 {
+	
 	int size;
-    cout<<"enter the size of matrix"<<endl;
-    cin>>size;
-    int arr[size];
-    cout<<"enter the array"<<endl;
-    for(int i=0;i<size;i++)
-    {
-        cin>>arr[i];
-    }
+	cout<<"Enter size of array"<<endl;
+	cin>>size;
+	int arr[size];
+	cout<<"Enter elements\n";
+	for(int i=0; i<size; i++) cin>>arr[i];
+
 	cout << "Minimum number of multiplications is "
 		<< MatrixChainOrder(arr, size);
 
 	getchar();
 	return 0;
 }
-
 
